4cd565d47f87f5d79b40d53f2e8e106d0fdd2340
Mikhraly <m.gasratov@npomolniya.ru>
2022-03-16 09:09:34 +0300

    project created

---
+++ pp_bku/main.c
@@ -0,0 +1,18 @@
+/*
+ * pp_bku.c
+ *
+ * Created: 16.03.2022 8:44:17
+ * Author : m.gasratov
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
