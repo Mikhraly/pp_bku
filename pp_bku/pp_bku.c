6c4cab4d30a04d92a7563b39453700eac1dbda93
Mikhraly <m.gasratov@npomolniya.ru>
2022-03-28 13:41:37 +0300

    changed functions

--- pp_bku/pp_bku.c
+++ pp_bku/pp_bku.c
@@ -18,120 +18,132 @@ int main(void)
 	hd44780_printString1("GB1-? GB2-? GB3-? GB4-? GB5-? GB6-?");
 	hd44780_printString2("GB7-? УПС-? МРДТТ-? ПП1-? ПП2-?");
 	
+	Module gb1 = {0, 0, 1, 163, 2};	// 3.2V, 0.04V
+	Module gb2 = {0, 0, 1, 163, 2};	// 3.2V, 0.04V
+	Module gb3 = {0, 0, 1, 163, 2};	// 3.2V, 0.04V
+	Module gb4 = {0, 0, 1, 163, 2};	// 3.2V, 0.04V
+	Module gb5 = {0, 0, 1, 163, 2};	// 3.2V, 0.04V
+	Module gb6 = {0, 0, 1, 163, 2};	// 3.2V, 0.04V
+	Module gb7 = {0, 0, 1, 163, 2};	// 3.2V, 0.04V
+	Module ups = {0, 0, 2, 112, 1};	// 2.2V, 0.02V
+	Module mrdtt = {0, 0, 2, 112, 1};	// 2.2V, 0.02V
+	Module pp1 = {0, 0, 0, 51, 2};	// 1V, 0.04V
+	Module pp2 = {0, 0, 0, 51, 2};	// 1V, 0.04V
+	
 	while (1) {
 		
 		if (~PINB & 1<<4) {		// ПП1
-			status.pp1 = checkAdcResult(adcMeasureAverageOfTen(0), 51, 2);	// 1V, 0.04V
-			flag.pp1Worked = 1;
+			pp1.status = checkModuleStatus(&pp1);
+			pp1.worked = 1;
 			while (~PINB & 1<<4);
 			_delay_us(10);
 		}
 		if (~PIND & 1<<0) {		// ПП2
-			status.pp2 = checkAdcResult(adcMeasureAverageOfTen(0), 51, 2);	// 1V, 0.04V
-			flag.pp2Worked = 1;
+			pp2.status = checkModuleStatus(&pp2);
+			pp2.worked = 1;
 			while (~PIND & 1<<0);
 			_delay_us(10);
 		}
 		if (~PIND & 1<<1) {		// GB5
-			status.gb5 = checkAdcResult(adcMeasureAverageOfTen(1), 163, 2);	// 3.2V, 0.04V
-			flag.gb5Worked = 1;
+			gb5.status = checkModuleStatus(&gb5);
+			gb5.worked = 1;
 			while (~PIND & 1<<1);
 			_delay_us(10);
 		}
 		if (~PIND & 1<<2) {		// GB6
-			status.gb6 = checkAdcResult(adcMeasureAverageOfTen(1), 163, 2);	// 3.2V, 0.04V
-			flag.gb6Worked = 1;
+			gb6.status = checkModuleStatus(&gb6);
+			gb6.worked = 1;
 			while (~PIND & 1<<2);
 			_delay_us(10);
 		}
 		if (~PIND & 1<<3) {		// GB7
-			status.gb7 = checkAdcResult(adcMeasureAverageOfTen(1), 163, 2);	// 3.2V, 0.04V
-			flag.gb7Worked = 1;
+			gb7.status = checkModuleStatus(&gb7);
+			gb7.worked = 1;
 			while (~PIND & 1<<3);
 			_delay_us(10);
 		}
 		if (~PIND & 1<<4) {		// GB1
-			status.gb1 = checkAdcResult(adcMeasureAverageOfTen(1), 163, 2);	// 3.2V, 0.04V
-			flag.gb1Worked = 1;
+			gb1.status = checkModuleStatus(&gb1);
+			gb1.worked = 1;
 			while (~PIND & 1<<4);
 			_delay_us(10);
 		}
 		if (~PIND & 1<<5) {		// GB2
-			status.gb2 = checkAdcResult(adcMeasureAverageOfTen(1), 163, 2);	// 3.2V, 0.04V
-			flag.gb2Worked = 1;
+			gb2.status = checkModuleStatus(&gb2);
+			gb2.worked = 1;
 			while (~PIND & 1<<5);
 			_delay_us(10);
 		}
 		if (~PIND & 1<<6) {		// GB3
-			status.gb3 = checkAdcResult(adcMeasureAverageOfTen(1), 163, 2);	// 3.2V, 0.04V
-			flag.gb3Worked = 1;
+			gb3.status = checkModuleStatus(&gb3);
+			gb3.worked = 1;
 			while (~PIND & 1<<6);
 			_delay_us(10);
 		}
 		if (~PIND & 1<<7) {		// GB4
-			status.gb4 = checkAdcResult(adcMeasureAverageOfTen(1), 163, 2);	// 3.2V, 0.04V
-			flag.gb4Worked = 1;
+			gb4.status = checkModuleStatus(&gb4);
+			gb4.worked = 1;
 			while (~PIND & 1<<7);
 			_delay_us(10);
 		}
 		if (~PINC & 1<<0) {		// МРДТТ
-			status.mrdtt = checkAdcResult(adcMeasureAverageOfTen(2), 112, 1);	// 2.2V, 0.02V
-			flag.mrdttWorked = 1;
+			mrdtt.status = checkModuleStatus(&mrdtt);
+			mrdtt.worked = 1;
 			while (~PINC & 1<<0);
 			_delay_us(10);
 		}
 		if (~PINC & 1<<1) {		// УПС
-			status.ups = checkAdcResult(adcMeasureAverageOfTen(2), 112, 1);	// 2.2V, 0.02V
-			flag.upsWorked = 1;
+			ups.status = checkModuleStatus(&ups);
+			ups.worked = 1;
 			while (~PINC & 1<<1);
 			_delay_us(10);
 		}
 		
 		
-		if (flag.pp1Worked) {
-			flag.pp1Worked = 0;
+		if (pp1.worked) {
+			pp1.worked = 0;
 			hd44780_setAddress(0x18);
-			hd44780_print(status.pp1);
+			hd44780_print(pp1.status);
 		}
-		if (flag.pp2Worked) {
-			flag.pp2Worked = 0;
+		if (pp2.worked) {
+			pp2.worked = 0;
 			hd44780_setAddress(0x20);
-			hd44780_print(status.pp2);
+			hd44780_print(pp2.status);
 		}
-		if (flag.gb5Worked) {
-			flag.gb5Worked = 0;
+		if (gb5.worked) {
+			gb5.worked = 0;
 			hd44780_setAddress(0x1E);
-			hd44780_print(status.gb5);
+			hd44780_print(gb5.status);
 		}
-		if (flag.gb6Worked) {
-			flag.gb6Worked = 0;
+		if (gb6.worked) {
+			gb6.worked = 0;
 			hd44780_setAddress(0x24);
-			hd44780_print(status.gb6);
+			hd44780_print(gb6.status);
 		}
-		if (flag.gb7Worked) {
-			flag.gb7Worked = 0;
+		if (gb7.worked) {
+			gb7.status = 0;
 			hd44780_setAddress(0x40);
-			hd44780_print(status.gb7);
+			hd44780_print(gb7.status);
 		}
-		if (flag.gb1Worked) {
-			flag.gb1Worked = 0;
+		if (gb1.worked) {
+			gb1.worked = 0;
 			hd44780_setAddress(0x04);
-			hd44780_print(status.gb1);
+			hd44780_print(gb1.status);
 		}
-		if (flag.gb2Worked) {
-			flag.gb2Worked = 0;
+		if (gb2.worked) {
+			gb2.worked = 0;
 			hd44780_setAddress(0x0A);
-			hd44780_print(status.gb2);
+			hd44780_print(gb2.status);
 		}
-		if (flag.gb3Worked) {
-			flag.gb3Worked = 0;
+		if (gb3.worked) {
+			gb3.worked = 0;
 			hd44780_setAddress(0x12);
-			hd44780_print(status.gb3);
+			hd44780_print(gb3.status);
 		}
-		if (flag.gb4Worked) {
-			flag.gb4Worked = 0;
+		if (gb4.worked) {
+			gb4.worked = 0;
 			hd44780_setAddress(0x16);
-			hd44780_print(status.gb4);
+			hd44780_print(gb4.status);
 		}
 		
 	}
@@ -151,15 +163,14 @@ void ports_init() {
 	PORTD = 0xFF;
 }
 
-uint8_t adcMeasureAverageOfTen(uint8_t channel) {
+
+uint8_t checkModuleStatus(Module* module) {
 	uint16_t adcValue = 0;
 	for (uint8_t i=0; i<10; i++)
-		adcValue += (uint16_t)(adc_work_8bit(channel));
-	return (uint8_t)(adcValue /= 10);
-}
-
-uint8_t checkAdcResult(uint8_t adcResult, uint8_t minToLaunch, uint8_t minToPreparation) {
-	if (adcResult > minToLaunch) return 0xA4;				// "З" - Запуск
-	else if (adcResult > minToPreparation) return 0xA8;		// "П" - Проверка
-	else return 0x4F;										// "О" - Обрыв
+		adcValue += (uint16_t)(adc_work_8bit(module->adcChannel));
+	uint8_t adcAverage = (uint8_t)(adcValue /= 10);
+	
+	if (adcAverage > module->minToLaunch) return 0xA4;				// "З" - Запуск
+	else if (adcAverage > module->minToPreparation) return 0xA8;	// "П" - Проверка
+	else return 0x4F;												// "О" - Обрыв
 }
--- pp_bku/pp_bku.h
+++ pp_bku/pp_bku.h
@@ -17,37 +17,17 @@
 #include "hd44780.h"
 #include "adc.h"
 
-struct {
-	uint8_t gb1Worked	:1;
-	uint8_t gb2Worked	:1;
-	uint8_t gb3Worked	:1;
-	uint8_t gb4Worked	:1;
-	uint8_t gb5Worked	:1;
-	uint8_t gb6Worked	:1;
-	uint8_t gb7Worked	:1;
-	uint8_t upsWorked	:1;
-	uint8_t mrdttWorked	:1;
-	uint8_t pp1Worked	:1;
-	uint8_t pp2Worked	:1;
-} flag = {0};
+typedef struct {
+	uint8_t worked	:1;
+	uint8_t status	:1;
+	uint8_t adcChannel;
+	uint8_t minToLaunch;
+	uint8_t minToPreparation;
+} Module;
 
-struct {
-	uint8_t gb1;
-	uint8_t gb2;
-	uint8_t gb3;
-	uint8_t gb4;
-	uint8_t gb5;
-	uint8_t gb6;
-	uint8_t gb7;
-	uint8_t ups;
-	uint8_t mrdtt;
-	uint8_t pp1;
-	uint8_t pp2;
-} status = {0};
 
 void ports_init();
-uint8_t adcMeasureAverageOfTen(uint8_t channel);
-uint8_t checkAdcResult(uint8_t adcResult, uint8_t minToLaunch, uint8_t minToPreparation);
+uint8_t checkModuleStatus(Module* module);
 
 
 #endif /* PP_BKU_H_ */
